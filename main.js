319aa5920f30c3cc8611ddca451aa089854161e5
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-18 15:47:19 -0400

    lodash utility usage

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 
+const _ = require('lodash');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
@@ -19,13 +20,22 @@ app.on('ready', () => {
 
 
 ipcMain.on('addedVideos', (e, videos) => {
-  const promise = new Promise((resolve, reject) => {
-    ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
-      resolve(metadata);
+  // const promise = new Promise((resolve, reject) => {
+  //   ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
+  //     resolve(metadata);
+  //   });
+  // });
+  // promise.then((metadata) => {
+  //   console.log(metadata);
+  // });
+
+  //lodash utility
+  _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        resolve(metadata);
+      });
     });
-  });
-  promise.then((metadata) => {
-    console.log(metadata);
   });
 
 });
