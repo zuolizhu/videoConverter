3ad17f4f53fe111abc2c4c60ded911415685ad44
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-18 15:18:40 -0400

    send video list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import { ADD_VIDEO, ADD_VIDEOS, REMOVE_VIDEO, REMOVE_ALL_VIDEOS, VIDEO_PROGRESS, VIDEO_COMPLETE } from "./types";
+import { ipcRenderer } from 'electron';
 
 // TODO: Communicate to MainWindow process that videos
 // have been added and are pending conversion
 export const addVideos = videos => dispatch => {
-
+  ipcRenderer.send('addedVideos', videos);
 };
 
 
